34d7e652891e432ec8dd792fd8b83b94b1716ea2
Prish20 <prish@students.uonbi.ac.ke>
2024-05-19 04:32:16 +0300

    Create a middleware and a function to handle errors

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -1,17 +1,18 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password || username === "" || password === "") {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400, "All fields are required"));
   }
   const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = User({ username, email, password: hashedPassword});
+  const newUser = User({ username, email, password: hashedPassword });
   try {
-      await newUser.save();
-      res.json("User created successfully");
+    await newUser.save();
+    res.json("User created successfully");
   } catch (error) {
-    res.status(500).json({ message: error.message })
-  };
+    next(error);
+  }
 };
