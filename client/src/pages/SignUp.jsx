1f107cdc2c9f2df62afff5e314a7e33392c934f9
Prish20 <prish@students.uonbi.ac.ke>
2024-05-21 06:40:27 +0300

    Add funcionality to the signup page to interact with the backend

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,41 @@
-import { Link } from "react-router-dom";
+import { Alert, Button, Spinner } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill in all fields");
+    }
+    setLoading(true);
+    setErrorMessage(null);
+    try {
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
@@ -14,7 +49,7 @@ const SignUp = () => {
             <h2 className="text-2xl font-bold">Create an account</h2>
           </span>
         </div>
-        <form className="w-full">
+        <form className="w-full" onSubmit={handleSubmit}>
           <div className="mb-4">
             <label className="block text-gray-700">Username*</label>
             <input
@@ -22,6 +57,7 @@ const SignUp = () => {
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
               placeholder="Enter your name"
               id="username"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -31,6 +67,7 @@ const SignUp = () => {
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
               placeholder="Enter your email"
               id="email"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4">
@@ -40,15 +77,24 @@ const SignUp = () => {
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
               placeholder="Enter your password"
               id="password"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-4"></div>
-          <button
+          <Button
             type="submit"
             className="w-full bg-pink-500 text-white py-2 rounded-lg hover:bg-pink-600"
+            disabled={loading}
           >
-            Sign Up
-          </button>
+            {loading ? (
+              <>
+                <Spinner size="sm" />
+                <span className="pl-3">Loading...</span>
+              </>
+            ) : (
+              "Sign Up"
+            )}
+          </Button>
         </form>
         <div className="text-center mt-4">
           <p>
@@ -58,6 +104,11 @@ const SignUp = () => {
             </Link>
           </p>
         </div>
+        {errorMessage && (
+          <Alert className="mt-5" color="failure">
+            {errorMessage}
+          </Alert>
+        )}
       </div>
     </div>
   );
