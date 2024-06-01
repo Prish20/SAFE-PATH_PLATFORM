81d6ad11b47d6c88227adff77c8839b571a10fb7
Prish20 <prish@students.uonbi.ac.ke>
2024-06-01 20:23:33 +0300

    Complete profile page functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,29 +1,52 @@
-import { Button, TextInput } from "flowbite-react";
-import { useSelector } from "react-redux";
+import { Alert, Button, TextInput } from "flowbite-react";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import {
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} from "../redux/user/userSlice.js";
+import { useDispatch } from "react-redux";
 
 export default function DashProfile() {
+  const [formData, setFormData] = useState({});
   const { currentUser } = useSelector((state) => state.user);
-  const [formData, setFormData] = useState({
-    username: currentUser?.username || "",
-    email: currentUser?.email || "",
-    password: "",
-  });
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(false);
+  const [updateUserError, setUpdateUserError] = useState(false);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Handle form submission logic here
-    console.log("Updated user data:", formData);
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError("No changes made!");
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.user));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess("User's profile updated successfully!");
+      }
+    } catch (error) {
+      dispatch(updateFailure(error.message));
+      setUpdateUserError(error.message);
+    }
   };
-
-  if (!currentUser) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -35,52 +58,44 @@ export default function DashProfile() {
             className="rounded-full w-full h-full border-8 border-[lightgray] object-cover"
           />
         </div>
-        <label htmlFor="username" className="sr-only">
-          Username
-        </label>
         <TextInput
           type="text"
           id="username"
           placeholder="username"
-          value={formData.username}
+          defaultValue={currentUser.username}
           onChange={handleChange}
-          className="rounded-full border-2 border-gray-300 text-center shadow-md w-full box-border"
         />
-        <label htmlFor="email" className="sr-only">
-          Email
-        </label>
         <TextInput
           type="email"
           id="email"
           placeholder="email"
-          value={formData.email}
+          defaultValue={currentUser.email}
           onChange={handleChange}
-          className="rounded-full border-2 border-gray-300 text-center shadow-md w-full box-border"
         />
-        <label htmlFor="password" className="sr-only">
-          Password
-        </label>
         <TextInput
           type="password"
           id="password"
           placeholder="password"
-          value={formData.password}
           onChange={handleChange}
-          className="rounded-full border-2 border-gray-300 text-center shadow-md w-full box-border"
         />
-        <Button
-          type="submit"
-          gradientDuoTone="purpleToBlue"
-          outline
-          className="rounded-full"
-        >
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
           Update
         </Button>
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span className="cursor-pointer">Delete Account</span>
-        <span className="cursor-pointer">Sign Out</span>
+        <span className="cursor-pointer">Sign OUt</span>
       </div>
+      {updateUserSuccess && (
+        <Alert type="success" className="mt-5">
+          {updateUserSuccess}
+        </Alert>
+      )}
+      {updateUserError && (
+        <Alert type="failure" className="mt-5">
+          {updateUserError}
+        </Alert>
+      )}
     </div>
   );
 }
