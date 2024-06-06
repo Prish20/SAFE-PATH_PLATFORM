c9d0e2f58f418c9e6173f30add9b53478915416e
Prish20 <prish@students.uonbi.ac.ke>
2024-06-06 14:05:51 +0300

    Create get users api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { getUsers, test, updateUser } from "../controllers/user.controller.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
 router.put("/update/:userId", verifyUser, updateUser);
+router.get("/getusers", verifyUser, getUsers);
 
 export default router;
