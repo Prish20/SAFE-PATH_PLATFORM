3b6d57d6a1f19b015fddd832a98b22b3752b65f7
Prish20 <prish@students.uonbi.ac.ke>
2024-06-07 16:44:04 +0300

    Add pre-installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import postRoutes from "./routes/post.route.js";
 import incidentRoutes from "./routes/incident.routes.js";
+import path from "path";
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose
     console.log("Mongodb is connected!");
   })
   .catch((err) => console.log(err));
+  const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +30,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/incident", incidentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
